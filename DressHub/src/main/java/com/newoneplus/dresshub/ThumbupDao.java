1319e1dcaab9ba3d2c1ebe40c4ffad0f1f658e7a
voidblue <kimqwe12300@daum.net>
2018-04-25 21:48:37 +0900

    revise

--- DressHub/src/main/java/com/newoneplus/dresshub/ThumbupDao.java
+++ DressHub/src/main/java/com/newoneplus/dresshub/ThumbupDao.java
@@ -11,9 +11,9 @@ import java.sql.PreparedStatement;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-public class ThumbUpDao {
+public class ThumbupDao {
     private JdbcTemplate jdbcTemplate;
-    public ThumbUpDao(JdbcTemplate jdbcTemplate) {
+    public ThumbupDao(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -37,7 +37,7 @@ public class ThumbUpDao {
     }
 
     //유저가 좋아요를 누른 상품들의 id를 반환하는 기능
-    public ArrayList<Integer> getLikeProductIdList(int userId){
+    public ArrayList<Integer> getLikeProductIdList(String userId){
         Object[] params = {userId};
         ArrayList<Integer> productIds = null;
         try {
@@ -46,7 +46,7 @@ public class ThumbUpDao {
                         ArrayList<Integer> arrayList = new ArrayList();
                         while(rs.next()){
                             //TODO 아직 PRODUCT ID가 이름이 없음  , 왜래키 그냥 이대로 가져올 수 있는지도 모름
-                            arrayList.add(rs.getInt("PRODUCT_ID"));
+                            arrayList.add(rs.getInt("PRODUCT"));
                         }
                         return arrayList;
                     });
@@ -58,14 +58,14 @@ public class ThumbUpDao {
 
 
     //좋아요 누르고 좋아요가 생성되어야 하는 경우 호출
-    public Integer insert(int userId, int productId) {
+    public Integer insert(String userId, int productId) {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         Object[] params = {userId, productId};
         jdbcTemplate.update(con -> {
             @Cleanup
             PreparedStatement preparedStatement = con.prepareStatement(
                     //TODO 아직 PRODUCT ID가 이름이 없음  , 왜래키 그냥 이대로 가져올 수 있는지도 모름
-                    "INSERT THUMB_UP(USER_ID, PRODUCT_ID) VALUES (?, ?)",
+                    "INSERT THUMB_UP(USER, PRODUCT) VALUES (?, ?)",
                     Statement.RETURN_GENERATED_KEYS);
             for (int i = 0; i < params.length; i++) {
                 preparedStatement.setObject(i + 1, params[i]);
@@ -77,9 +77,9 @@ public class ThumbUpDao {
     }
 
     //좋아요를 철회하는 기능
-    public void delete(int userId, int productId){
+    public void delete(String userId, int productId){
         Object[] params = {userId, productId};
-        jdbcTemplate.update("DELETE FROM THUMB_UP WHERE USER_ID = ? AND PRODUCT_ID = ?", params);
+        jdbcTemplate.update("DELETE FROM THUMB_UP WHERE USER = ? AND PRODUCT = ?", params);
     }
 
 
