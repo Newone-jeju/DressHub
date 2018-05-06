c798812bc8bbac2efb61f460ad1bd2e8b8373e0a
unknown <dldhwn56@gmail.com>
2018-05-06 14:16:32 +0900

    review write

--- front/js/review-form.js
+++ front/js/review-form.js
@@ -1,32 +1,42 @@
-if(window.opener.$("#product-details h1.title:eq(0)").text() != null) {
+function getName(){
 	$('.rental-product-content').text(window.opener.$("#product-details h1.title:eq(0)").text());
+	console.log($('.rental-product-content').text());
+	if($('.rental-product-content').text() ==''){
+		alert('잘못된 경로로 접근');
+		window.open('about:blank', '_self').close();
+	}
 }
-else{
 
+function setHiddenName(){
+	$('.rental-product-content').after('<input type="hidden" name="rental-product" value="'+$('.rental-product-content').text()+'">')
+}
+
+function setHiddenRating(){
+	$("#rating").rateYo({
+		starWidth: "30px",
+		halfStar: true,
+  onSet: function (rating, rateYoInstance) {
+    $("#rating").after('<input type="hidden" name="rank" value="'+rating+'">');
+  }
+});
+} 
+
+function reviewFormInit() {
+	getName();
+	setHiddenName();
+	setHiddenRating();
 }
 
 
-
-$("#rating").rateYo({
-            starWidth: "30px",
-            halfStar: true
-        });
+reviewFormInit();
 
 
-$("send-btn").click(function(){
+
+$(".send-btn").click(function(){
 	opener.parent.location.reload();
-	window.close(); 
-	self.close(); 
-	window.opener = window.location.href; 
-	self.close(); 
-	window.open('about:blank','_self').close();
-	
+	window.open('about:blank', '_self').close();	
 });
 
-$("cancel-btn").click(function(){
-	window.close(); 
-	self.close(); 
-	window.opener = window.location.href; 
-	self.close(); 
-	window.open('about:blank','_self').close();
+$(".cancel-btn").click(function(){
+	window.open('about:blank', '_self').close();
 });
