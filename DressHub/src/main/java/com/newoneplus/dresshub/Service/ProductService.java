3fc5250bfd37c0ef14364467fed69331e0955c4a
orionjeong <djflqjfl0209@gmail.com>
2018-05-10 00:51:18 +0900

    imageupload 처리로직 controller에서 service로 이관

--- DressHub/src/main/java/com/newoneplus/dresshub/Service/ProductService.java
+++ DressHub/src/main/java/com/newoneplus/dresshub/Service/ProductService.java
@@ -1,6 +1,7 @@
 package com.newoneplus.dresshub.Service;
 
 
+import com.newoneplus.dresshub.ImageProcesser;
 import com.newoneplus.dresshub.Model.Product;
 import com.newoneplus.dresshub.Model.ProductDao;
 import com.newoneplus.dresshub.Model.ProductImage;
@@ -8,6 +9,11 @@ import com.newoneplus.dresshub.Model.ProductImageDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -20,14 +26,55 @@ public class ProductService {
     private ProductImageDao productImageDao;
 
 
-    public void insertProductImage(ProductImage productImage) {
-       int a =productImageDao.insert(productImage);
-    }
+    public void insertProduct(Product product) throws IOException {
+        ImageProcesser imageProcesser = new ImageProcesser();
+        int product_id = productDao.insert(product);
+        product.setId(product_id);
+        String filename = "";
+        // 첨부파일(상품사진)이 있으면
+        if (!product.getImage().get(0).isEmpty()) {
+            for (int i = 0; i < product.getImage().size(); i++) {
 
-    public Integer insertProduct(Product product) {
-       return productDao.insert(product);
-    }
+                filename = product.getImage().get(i).getOriginalFilename();
+                String path = System.getProperty("user.dir") + "/src/main/resources/static/product_image/";
+                ProductImage productImage = new ProductImage();
+                productImage.setProductId(product.getId());
+//              맨처음 파일만   파일 크기 3가지로 구분 해서 db및 file 저장
+                if (i == 0) {
+                    try {
+                        product.setThumbnailImage("origin" + filename);
+                        BufferedImage image = imageProcesser.getMediumImage(product.getImage().get(i).getInputStream());
+                        String mediumpath = "medium" + filename;
+                        ImageIO.write(image, "jpg", new File(path + mediumpath));
+                        productImage.setImage(mediumpath);
+                        productImage.setImageSize("중간");
+                        productImageDao.insert(productImage);
+                        BufferedImage image2 = imageProcesser.getSmallImage(product.getImage().get(i).getInputStream());
+                        String smallpath = "small" + filename;
+                        ImageIO.write(image2, "jpg", new File(path + smallpath));
+                        productImage.setImage(smallpath);
+                        productImage.setImageSize("작은");
+                        productImageDao.insert(productImage);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
 
+                    try {
+                        new File(path).mkdirs(); // 디렉토리 생성
+                        // 임시디렉토리(서버)에 저장된 파일을 지정된 디렉토리로 전송
+                        String originpath = "origin" + filename;
+                        product.getImage().get(i).transferTo(new File(path + originpath));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                productImage.setImage("origin" + filename);
+                productImage.setImageSize("일반");
+                productImageDao.insert(productImage);
+            }
+            productDao.update(product);
+        }
+    }
     public void updateProduct(Product product){
         productDao.update(product);
     }
