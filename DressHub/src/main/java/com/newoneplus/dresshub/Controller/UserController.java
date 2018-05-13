2e44c45a8c7377f91100fadce7f637f13232e930
Lming1 <spear131121@gmail.com>
2018-05-13 22:56:31 +0900

    User id, password, email 검증 추가

--- DressHub/src/main/java/com/newoneplus/dresshub/Controller/UserController.java
+++ DressHub/src/main/java/com/newoneplus/dresshub/Controller/UserController.java
@@ -8,10 +8,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -29,18 +31,13 @@ public class UserController {
     }
 
     @PostMapping("")
-    public String create(HttpServletRequest httpServletRequest) throws ClassNotFoundException, ParseException {
+    public String create(@ModelAttribute @Valid @NotNull() User user) throws ClassNotFoundException, ParseException {
 
-        User user = new User();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date = null;
         date = simpleDateFormat.parse(simpleDateFormat.format(new Date()));
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setId(httpServletRequest.getParameter("id"));
-        user.setPassword(passwordEncoder.encode(httpServletRequest.getParameter("password")));
-        user.setName(httpServletRequest.getParameter("name"));
-        user.setEmail(httpServletRequest.getParameter("email"));
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setUserType(1);
         user.setResisterDate(date);
         userService.save(user);
