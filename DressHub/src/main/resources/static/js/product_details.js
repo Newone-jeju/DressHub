f377b026966d30ddb19e91fdd83152d722f3891d
DESKTOP-07203FE\ojlee <dldhwn56@gmail.com>
2018-06-10 21:52:37 +0900

    상품 상세정보 완

--- DressHub/src/main/resources/static/js/product_details.js
+++ DressHub/src/main/resources/static/js/product_details.js
@@ -1,26 +1,31 @@
 (function(){
 
-    var ajaxData ="";
-    var ajaxCard ="";
-
-    var data =[];
-
-    function getURLParameter(name) {
-        return decodeURI(
-         (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
-        );
+    // function getURLParameter(name) {
+    //     return decodeURI(
+    //      (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
+    //     );
+    // }
+    function getURLId(url){
+        return url.match(/\/([^\/]+)\/?$/)[1];
     }
 
-
-
-
-    function initMap(url){
-
-        //데이터 가져오고
-        ajaxData = new AjaxData(url, false);
+    //데이터 가져오기
+    function dataInit(){
+        var productId = getURLId(document.location.href);
+        var ajaxData = '';
+        //상품 데이터
+        ajaxData = new AjaxData('products/'+productId+, false);
         data = ajaxData.getData()[0];
         ajaxData = undefined;
+        //이미지 데이터
+        var size = ['origin', 'medium', 'small'];
+        for(var i = 0; i<size.length; i++){
+            ajaxData = new AjaxData('/products/image/image/'+size[i]+data.name+'.jpg', false);
+            Object.keys(ajaxImg)[i] = ajaxData.getDate();
+        }
+    }
 
+    function initMap(){
         //상품 헤더 정보 카테고리, 이름
         $(".page-head-category span.category2").text(data.category);
         $(".productName").text(data.name);
@@ -62,11 +67,11 @@
             },
             {
                 target: ".product > .size",
-                value: data.size  // 이름 변경 필요
+                value: data.size
             },
             {
                 target: ".product > .location",
-                value: data.location // 없음
+                value: data.location
             },
             {
                 target: ".price > .deposit",
@@ -100,7 +105,7 @@
         ajaxCard.mapCard($("article-area .body"));
         
         ajaxCard = undefined;
-    }
+    }    
 
     // 대형 이미지 매핑
     function bigMap(i){
@@ -116,31 +121,75 @@
 
     }
 
-    //초기 매핑
-    initMap("js/productData.json", false); // init mapping url 필요
+    //날짜 차이 계산
+    function calDay(){
+        var sDay = $("#s-date").val();
+        var eDay = $("#e-date").val();
+        var diffDay = "";
+        sDay = new Date(sDay);
+        eDay = new Date(eDay);
+        if(diffDay>=0){
+            alert("날짜입력오류: 시작일보다 종료일이 늦습니다");
+            return false;
+        }
+        diffDay = (eDay.getTime() - sDay.getTime())/1000/60/60/24;
+        return diffDay;
+    }
 
-    //주문 장바구니 버튼
+    //상품이름, 배송비등 보내고 주문 금액 계산후 세션 스토리지 저장
+    function sessionPrice(){
+        var totalPrice = calDay()*data.costPerDay
+        var deposit = data.deposit;
+        var totalShipment = 3000;
+        var totalPayment = deposit+totalShipment+totalPrice;
+
+        sessionStorage.setItem("productId", data.id);
+        sessionStorage.setItem("productName", data.name);
+        sessionStorage.setItem("totalPrice", totalPrice);
+        sessionStorage.setItem("totalShipment", 3000);
+        sessionStorage.setItem("totalPayment", totalPayment);
+    }
+
+    var ajaxCard ="";
+    var ajaxImg = {
+        origin: "",
+        medium: "",
+        small: ""
+    };
+    var data =[];
+    
+    //데이터 가져오기
+    dataInit();
+    //초기 매핑
+    initMap(); // init mapping url 필요
+
+    //주문 버튼
     $(".submit-btn > button.request").click(function(){
-        var ajaxUtil = new AjaxUtil(); // 주문URL 필요
-        var postData = {
-            id: data.id,
-            lease_day: $("#s-date").attr("value"),
-            return_day: $("#e-date").attr("value")
-        };
+        var user = new CookieUser();
+        user.inspectId();
+        sessionPrice();
         AjaxUtil.crudData(postData, "POST", function(){
             window.location.href = '/order.html';//결제페이지
         })
+        
     })
 
+    //장바구니 버튼
     $(".submit-btn > button.add-cart").click(function(){
-        var ajaxUtil = new AjaxUtil(); // 장바구니URL 필요
+        var user = new CookieUser();
+        var userId = user.getUserId();
+        user.inspectId();
+
+        var ajaxUtil = new AjaxUtil('/baskets'); // 장바구니URL 필요
         var postData = {
-            id: data.id
+            id: data.id,
+            userId: userId
         };
         AjaxUtil.crudData(postData, "POST", function(){
             window.location.href = '/cart.html';//장바구니 페이지
         })
     })
+
 
 })();
 
