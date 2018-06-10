9c0e7db6d8e6ee4f9c46ae4da769d13d5dee3384
soyoung1394 <syoung1394@naver.com>
2018-06-10 23:00:49 +0900

    order 수정

--- DressHub/src/main/resources/static/js/order.js
+++ DressHub/src/main/resources/static/js/order.js
@@ -1,45 +1,65 @@
-$(document).ready(function () {
-                 $.ajax({
-                url: '/basketList?userId=user1',
-                dataType: 'json',
-                type: 'get',
-                success: function (data) {
-                    var product = {};
+// sessionStorage.setItem("productName", "가나다");
+// sessionStorage.setItem("totalPrice", 2000 );
+// sessionStorage.setItem("totalShipment", 3000);
+// sessionStorage.setItem("deposit", 7000);
 
-                    data = data.product;
-                    product.mapcard = function () {
-                        var cards = ' <tr>\n' +
-                            '      <th style="width: 10%;">이미지</th>\n' +
-                            '      <th>상품정보</th>\n' +
-                            '      <th>상품금액</th>\n' +
-                            '      <th>배송비</th>\n' +
-                            '    </tr>';
-                        for (var i = 0; i < data.length; i++) {
-                            cards +=
-                                                '<tr>'+
-                                                   '<td><img style="width:100%"src="./product_image/'+data[i].thumbnailImage+'"/></td>'+
-                                                   '<td>'+data[i].name+'</td>'+
-                                                   '<td>'+data[i].costPerDay+'</td>'+
-                                                   '<td>'+data[i].salePrice+'</td>'+
-                                                '</tr>'
-                        }
-                        var totalPrice=0;
-                        for(var i=0; i<data.length; i++){
-                            totalPrice+=data[i].costPerDay;
-                        }
-                        var totalShipping=0;
-                        for(var i=0; i<data.length; i++){
-                            totalShipping+=data[i].salePrice;
-                        }
-                        $('.total_price').html(totalPrice);
-                        $('.total_shipping').html(totalShipping);
-                        $('.payment_amount').html(totalPrice+totalShipping);
 
-                        $('.cartAdd_container').html(cards);
-                        $('#cart_table').html(cards);
+var productName  = sessionStorage.getItem('productName');
+var totalPrice   = sessionStorage.getItem('totalPrice');
+var totalShipment   = sessionStorage.getItem('totalShipment');
+var deposit   = sessionStorage.getItem('deposit');
+var totalPayment= parseInt(totalPrice)+parseInt(totalShipment)+parseInt(deposit);
 
-                    }
-                    product.mapcard();
-                }
-            })
-        });
+$('.productName').html(productName);
+$('.deposit').html(deposit);
+$('.totalPrice').html(totalPrice);
+$('.totalShipment').html(totalShipment);
+$('.totalPayment').html(totalPayment);
+
+
+
+
+// $(document).ready(function () {
+//                  $.ajax({
+//                 url: '/basketList?userId=user1',
+//                 dataType: 'json',
+//                 type: 'get',
+//                 success: function (data) {
+//                     var product = {};
+//
+//                     data = data.product;
+//                     product.mapcard = function () {
+//                         var cards = ' <tr>\n' +
+//                             '      <th>상품명</th>\n' +
+//                             '      <th>상품금액</th>\n' +
+//                             '      <th>배송비</th>\n' +
+//                             '    </tr>';
+//                         for (var i = 0; i < data.length; i++) {
+//                             cards +=
+//                                                 '<tr>'+
+//                                                    '<td><img style="width:100%"src="./product_image/'+data[i].thumbnailImage+'"/></td>'+
+//                                                    '<td>'+data[i].name+'</td>'+
+//                                                    '<td>'+data[i].costPerDay+'</td>'+
+//                                                    '<td>'+data[i].salePrice+'</td>'+
+//                                                 '</tr>'
+//                         }
+//                         var totalPrice=0;
+//                         for(var i=0; i<data.length; i++){
+//                             totalPrice+=data[i].costPerDay;
+//                         }
+//                         var totalShipping=0;
+//                         for(var i=0; i<data.length; i++){
+//                             totalShipping+=data[i].salePrice;
+//                         }
+//                         $('.total_price').html(totalPrice);
+//                         $('.total_shipping').html(totalShipping);
+//                         $('.payment_amount').html(totalPrice+totalShipping);
+//
+//                         $('.cartAdd_container').html(cards);
+//                         $('#cart_table').html(cards);
+//
+//                     }
+//                     product.mapcard();
+//                 }
+//             })
+//         });
