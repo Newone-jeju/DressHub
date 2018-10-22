ddfd98688adc80beb0b35a82af49aa8b763da90a
Lming1 <spear131121@gmail.com>
2018-10-23 00:29:50 +0900

    product image controller / add rest controller

--- DressHub/src/main/java/com/newoneplus/dresshub/Controller/ProductImageController.java
+++ DressHub/src/main/java/com/newoneplus/dresshub/Controller/ProductImageController.java
@@ -7,7 +7,9 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @RequestMapping("/")
 public class ProductImageController {
     private ResourceLoader resourceLoader = new DefaultResourceLoader();
