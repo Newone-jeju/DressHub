dd58d97664bc754444912f3128862c84f437528e
Lming1 <spear131121@gmail.com>
2018-10-27 20:39:20 +0900

    user api delete

--- DressHub/src/main/java/com/newoneplus/dresshub/Controller/RestUserController.java
+++ DressHub/src/main/java/com/newoneplus/dresshub/Controller/RestUserController.java
@@ -1,35 +1,35 @@
-package com.newoneplus.dresshub.Controller;
-
-
-import com.newoneplus.dresshub.Config.AuthProvider;
-import com.newoneplus.dresshub.Config.MyAuthentication;
-import com.newoneplus.dresshub.Model.User;
-import com.newoneplus.dresshub.Repository.UserRepository;
-import com.newoneplus.dresshub.Service.AuthorizationService;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
-import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpSession;
-
-@RestController
-@RequestMapping("/api/user")
-@RequiredArgsConstructor
-@Slf4j
-public class RestUserController {
-    private final UserRepository userRepository;
-    @GetMapping("/{uid}")
-//    @Secured("ROLE_USER")
-    public User get(@PathVariable String uid) {
-        return userRepository.findByUid(uid);
-//        return userRepository.findById(id).get();
-    }
-
-
-}
+//package com.newoneplus.dresshub.Controller;
+//
+//
+//import com.newoneplus.dresshub.Config.AuthProvider;
+//import com.newoneplus.dresshub.Config.MyAuthentication;
+//import com.newoneplus.dresshub.Model.User;
+//import com.newoneplus.dresshub.Repository.UserRepository;
+//import com.newoneplus.dresshub.Service.AuthorizationService;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.security.access.annotation.Secured;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.context.SecurityContextHolder;
+//import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+//import org.springframework.web.bind.annotation.*;
+//
+//import javax.servlet.http.HttpSession;
+//
+//@RestController
+//@RequestMapping("/api/user")
+//@RequiredArgsConstructor
+//@Slf4j
+//public class RestUserController {
+//    private final UserRepository userRepository;
+//    @GetMapping("/{uid}")
+////    @Secured("ROLE_USER")
+//    public User get(@PathVariable String uid) {
+//        return userRepository.findByUid(uid);
+////        return userRepository.findById(id).get();
+//    }
+//
+//
+//}
