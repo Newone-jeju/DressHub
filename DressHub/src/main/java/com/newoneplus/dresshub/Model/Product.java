21bd5a962542c4e2a0d57346afa13cfc9c103398
raphaelming <spear131121@gmail.com>
2020-12-23 23:08:46 +0900

    spring boot version 2.0.1 -> 2.3.5 / html 공통코드 분리(script, header, quick menu, navigation

--- DressHub/src/main/java/com/newoneplus/dresshub/Model/Product.java
+++ DressHub/src/main/java/com/newoneplus/dresshub/Model/Product.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Data
 @Entity
+
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -41,50 +42,50 @@ public class Product {
     @JsonIgnoreProperties(value = "product", allowSetters = true)
     List<ThumbUp> thumbUps;
 
-    @Override
-    public boolean equals(Object o){
-        boolean isEqual = true;
-
-        //아이디 빼고 모든 자료형..
-        if (o instanceof Product){
-            if(!name.equals(((Product) o).getName())){
-                isEqual = false;
-            }else if(!thumbnailImage.equals(((Product) o).getThumbnailImage())){
-                isEqual = false;
-            }else if(!contents.equals(((Product) o).getContents())){
-                isEqual = false;
-            }else if(!costPerDay.equals(((Product) o).getCostPerDay())){
-                isEqual = false;
-            }else if(!deposit.equals(((Product) o).getDeposit())){
-                isEqual = false;
-            }else if(!salePrice.equals(((Product) o).getSalePrice())){
-                isEqual = false;
-            }else if(!category.equals(((Product) o).getCategory())){
-                isEqual = false;
-            }else if(!consigmentStart.equals(((Product) o).getConsigmentStart())){
-                isEqual = false;
-            }else if(!consigmentEnd.equals(((Product) o).getConsigmentEnd())){
-                isEqual = false;
-            }else if(!state.equals(((Product) o).getState())){
-                isEqual = false;
-            }else if(!deleveryType.equals(((Product) o).getDeleveryType())){
-                isEqual = false;
-            }else if(!provider.equals(((Product) o).getProvider())){
-                isEqual = false;
-            }else if(!likes.equals(((Product) o).getLikes())){
-                isEqual = false;
-            }else if(!registrationDate.equals(((Product) o).getRegistrationDate())){
-                isEqual = false;
-            }else if(!leastLeaseDay.equals(((Product) o).getLeastLeaseDay())){
-                isEqual = false;
-            }else if(!size.equals(((Product) o).getSize())){
-                isEqual = false;
-            }
-        }
-        else{
-            isEqual = false;
-        }
-        return isEqual;
-    }
+//    @Override
+//    public boolean equals(Object o){
+//        boolean isEqual = true;
+//
+//        //아이디 빼고 모든 자료형..
+//        if (o instanceof Product){
+//            if(!name.equals(((Product) o).getName())){
+//                isEqual = false;
+//            }else if(!thumbnailImage.equals(((Product) o).getThumbnailImage())){
+//                isEqual = false;
+//            }else if(!contents.equals(((Product) o).getContents())){
+//                isEqual = false;
+//            }else if(!costPerDay.equals(((Product) o).getCostPerDay())){
+//                isEqual = false;
+//            }else if(!deposit.equals(((Product) o).getDeposit())){
+//                isEqual = false;
+//            }else if(!salePrice.equals(((Product) o).getSalePrice())){
+//                isEqual = false;
+//            }else if(!category.equals(((Product) o).getCategory())){
+//                isEqual = false;
+//            }else if(!consigmentStart.equals(((Product) o).getConsigmentStart())){
+//                isEqual = false;
+//            }else if(!consigmentEnd.equals(((Product) o).getConsigmentEnd())){
+//                isEqual = false;
+//            }else if(!state.equals(((Product) o).getState())){
+//                isEqual = false;
+//            }else if(!deleveryType.equals(((Product) o).getDeleveryType())){
+//                isEqual = false;
+//            }else if(!provider.equals(((Product) o).getProvider())){
+//                isEqual = false;
+//            }else if(!likes.equals(((Product) o).getLikes())){
+//                isEqual = false;
+//            }else if(!registrationDate.equals(((Product) o).getRegistrationDate())){
+//                isEqual = false;
+//            }else if(!leastLeaseDay.equals(((Product) o).getLeastLeaseDay())){
+//                isEqual = false;
+//            }else if(!size.equals(((Product) o).getSize())){
+//                isEqual = false;
+//            }
+//        }
+//        else{
+//            isEqual = false;
+//        }
+//        return isEqual;
+//    }
 }
 
--- DressHub/src/main/java/com/newoneplus/dresshub/Model/User.java
+++ DressHub/src/main/java/com/newoneplus/dresshub/Model/User.java
@@ -7,9 +7,6 @@ import org.hibernate.annotations.CreationTimestamp;
 
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
